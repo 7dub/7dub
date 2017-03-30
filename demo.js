fc11a23cde2c601323b23e2c4e012f0fc7ca6e19
柒色都吧 <7dub.cn@gmail.com>
2017-03-30 14:50:05 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -7,12 +7,15 @@ function hello() {
 // 	return JSON.stringify(obj);
 }
 
-function analyse() {
-	var obj ={
-		"page":"native home",
-		"uid":"v1456666",
-		"desc":"this is a string from 7dub"
-	};
+function analyse(data) {
+	var obj ={};
+	var bs = data.split("&");
+	if(bs.length > 0){
+		for(var i=0;i<bs.length;i++){
+			var item = bs[i].split("=");
+			obj.item[0] = item[1];
+		}
+	}
 // 	return JSON.stringify(obj);
 	return obj;
 }
